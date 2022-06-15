7b47603f42f71903edb8a832f4d9cc33764475a4
pylover <vahid.mardani@gmail.com>
2022-06-15 04:50:25 +0430

    Nothing

--- include/user_config.h
+++ include/user_config.h
@@ -7,7 +7,7 @@
 #define __version__ "0.1.0"
 
 #define PARAMS_DEFAULT_ZONE "dev"
-#define PARAMS_DEFAULT_NAME "esprelay"
+#define PARAMS_DEFAULT_NAME __name__
 #define PARAMS_DEFAULT_AP_PSK ""
 #define PARAMS_DEFAULT_STATION_SSID ""
 #define PARAMS_DEFAULT_STATION_PSK ""
